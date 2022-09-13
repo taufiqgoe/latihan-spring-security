219b46472d15f3ea0be23d7bc3ad0242f535e2c7
taufiq <taufiq.goe@gmail.com>
2022-09-13 10:37:37 +0800

    Initial commit

---
+++ src/main/java/id/taufiq/latihanspringsecurity/LatihanSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package id.taufiq.latihanspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatihanSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/taufiq/latihanspringsecurity/LatihanSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package id.taufiq.latihanspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
